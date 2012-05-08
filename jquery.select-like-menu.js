54870e2970d9f7a35f8b53ef17e6eb7b15437a7c
Petr Kessler <kesspess@gmail.com>
2012-05-08 09:58:39 +0200

    keyboard support added

--- jquery.select-like-menu.js
+++ jquery.select-like-menu.js
@@ -28,12 +28,32 @@ $.fn.extend({
 			}, o);
 
 		var $active = $this.children( options.activeSelector ).first(),
-			$placeholder = $(options.placeholder).html($active.clone().html()),
-			over = function () { $this.show(); },
+			$placeholder = $( options.placeholder ).html( $active.clone().html() ),
+			over = function () {
+				var $item = $(this);
+				$item.keydown(function (event) {
+					if (event.keyCode === 27 && !event.shiftKey && !event.altKey && !event.ctrlKey && !event.metaKey) {
+						$item.blur();
+					}
+				});
+
+				$this.show();
+			},
 			out = function () { $this.hide(); };
 
-		$placeholder.children().first().hover(over, out);
-		$this.hover(over, out).css('position', 'absolute').before( $placeholder ).hide();
+		$placeholder.children(':first')
+			.bind('mouseout blur', out)
+			.bind('mouseover focus', over);
+
+		$this.find('> li > *')
+			.bind('mouseout blur', out)
+			.bind('mouseover focus', over);
+
+		$this
+			.css('position', 'absolute')
+			.before( $placeholder )
+			.hide();
+
 		return $this;
 	}
 });
